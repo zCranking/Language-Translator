a861798369dac95c846c55fce8b91fa59dfbd15b
zCranking <aravgupta2019@gmail.com>
2022-11-22 15:41:30 -0800

    adding part 2

--- languagetrans.py
+++ languagetrans.py
@@ -5,3 +5,21 @@ from googletrans import Translator, LANGUAGES
 
 list = list(LANGUAGES.values())
 
+language1 = ""
+language2 = ""
+text = ""
+
+def Translate():
+    global language1, language2, text
+    language1 = combobox1.get()
+    language2 = combobox2.get()
+    text = text.get()
+
+translate = translate()
+
+translated = try: translate(text, 0, END, language1, language2)
+
+insert(END, translated.text)
+print("try again")
+button = (command = Translate())
+
